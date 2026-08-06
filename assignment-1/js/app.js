00f4cf591800e914a95cfc01f1bd77da3ba55df8
imcodes <ifmichael24@gmail.com>
2026-08-06 19:50:00 +0100

    Enhance task completion feature: add checkbox functionality to strike through completed tasks and update styles accordingly

--- assignment-1/js/app.js
+++ assignment-1/js/app.js
@@ -16,6 +16,14 @@ function addItem() {
             </li>`;
             // let li = "<li class='task'>" + task + "</li>";
             taskLists.innerHTML += li;
+
+            // Bind the event listener to the checkbox
+            let checkboxes = document.getElementsByClassName("task-checkbox");
+            // loop through the checkboxes and add event listener to each checkbox
+            for (let checkbox of checkboxes) {
+                checkbox.addEventListener('change', markTaskCompleted);
+            }
+  
         }
     }
 }
@@ -24,4 +32,25 @@ function addItem() {
 function toggleHighlight() {
     let tasksContainer = document.getElementById("tasksContainer");
     tasksContainer.classList.toggle("highlight");
+}
+
+/**
+ * Steps to strike through the completed tasks
+ * 
+ * 1. Target the checkbox element of each task item
+ * 2. Add an event listner to each of the checkboxes
+ * 3. When the checkbox is checked, add a class to the parent li element to strike through the text
+ * 4. When the checkbox is unchecked, remove the class from the parent li element to remove the strike through
+ * 5. Use CSS to style the completed tasks with a strike through effect
+ * 6. A function to bind the event listener to each checkbox when a new task is added
+ */
+// Function to handle checkbox change event (Strike Through)
+function markTaskCompleted(event) {
+    let elem = event.target; //targets the checkbox element
+    let taskItem = elem.parentElement;
+    taskItem.classList.toggle("completed"); 
+
+
+    console.log(`name: ${elem.tagName}, type: ${elem.type}, checked: ${elem.checked}`);
+    
 }
